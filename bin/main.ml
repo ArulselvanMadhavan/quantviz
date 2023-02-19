21b3ebc93786164ea043b639e940ca8401deba9a
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-02-18 20:22:30 -0800

    Add csv package

--- bin/main.ml
+++ bin/main.ml
@@ -10,6 +10,8 @@ let load_tensors ht filename =
   Hashtbl.update ht layer_name ~f:Layercontents.(update info_type contents)
 ;;
 
+let numel t = List.fold (Tensor.size t) ~init:1 ~f:Int.( * )
+
 let () =
   let dir_name = "/nfs/nomster/data/arul/data/artifacts/opt125m/fp32/layers/" in
   let files = Quantviz.Utils.dir_contents dir_name ~ext:"ot" in
@@ -17,6 +19,17 @@ let () =
   List.iter ~f:(load_tensors ht) files;
   Hashtbl.iteri ht ~f:(fun ~key ~data ->
     let l1, l2, l3 = Layercontents.lengths data in
+    List.iter data.outputs ~f:(fun (_, t) ->
+      let t = Tensor.abs_ t in
+      let ndims = List.length (Tensor.shape t) in
+      let x_max = Tensor.max_values t ~dim:(List.init ndims ~f:Fn.id) ~keepdim:false in
+      let x_max = Tensor.to_bigarray x_max ~kind:Bigarray.Float32 in
+      let x_max = Bigarray.array0_of_genarray x_max in
+      let x_max = Bigarray.Array0.get x_max in
+      let x_max = Scalar.f x_max in
+      let h_calib = Calibrator.Histogram.make_t ~num_bins:2048 ~x_max in
+      let hist = Calibrator.Histogram.collect h_calib t in
+      Stdio.printf "Tensor:%d|%d\n" (numel hist) (numel h_calib.calib_bin_edges));
     Stdio.printf "%s|%d|%d|%d\n" key l1 l2 l3);
   ()
 ;;
