8dbe555199f5ce07e87c6784ec913ab370623f33
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-04-09 07:40:15 -0700

    Save all

--- external/qtorch/core/qtensor_intf.ml
+++ external/qtorch/core/qtensor_intf.ml
@@ -1,14 +1,16 @@
-module type Tensor = module type of Torch_core.Wrapper.Tensor
-module type C = module type of Qtorch_bindings.C(Qtorch_generated)
+(* module type Tensor = module type of Torch_core.Wrapper.Tensor *)
+(* module type C = module type of Qtorch_bindings.C(Qtorch_generated) *)
     
-module type Qtensor = sig
-  type t
-  val tensor_to_int : t -> int
-end
+(* module type Qtensor = sig *)
+(*   type t *)
 
-module type Builder = sig
-  module Make(T : Tensor) : Qtensor with type t := T.t
-end
+(*   type t1 *)
+(*   val tensor_to_int : t -> int *)
+(* end *)
+
+(* module type Builder = sig *)
+(*   module Make(T : Tensor) : Qtensor with type t1 := T.t *)
+(* end *)
 
 (* module Qtensor(T : Tensor) = struct *)
 (*   include T *)
--- external/qtorch/stubs/qtorch_bindings.ml
+++ external/qtorch/stubs/qtorch_bindings.ml
@@ -17,15 +17,34 @@ open Ctypes
 (*   val tensor_to_int : (t -> int F.return) F.result *)
 (* end *)
 
-module C(F : Cstubs.FOREIGN) = struct
+(* module C(F : Cstubs.FOREIGN) = struct *)
+(*   open F *)
+
+(*   module Tensor = struct *)
+(*     type t = unit ptr *)
+
+(*     let t = ptr void *)
+
+(*     let tensor_to_int = foreign "tensor_to_int" (t @-> returning int) *)
+(*   end *)
+  
+(* end *)
+
+module type QB = functor (F : Cstubs.FOREIGN) -> sig
+  type t = Torch_core.Wrapper_generated.C.Tensor.t
+
+  val t : t typ
+
+  val tensor_to_int : (t -> int F.return) F.result
+end
+
+module C : QB = functor (F : Cstubs.FOREIGN) -> struct
   open F
 
-  module Tensor = struct
-    type t = unit ptr
+  type t = Torch_core.Wrapper_generated.C.Tensor.t
 
-    let t = ptr void
+  let t = Torch_core.Wrapper_generated.C.Tensor.t
 
-    let tensor_to_int = foreign "tensor_to_int" (t @-> returning int)
-  end
-  
+  let tensor_to_int = foreign "tensor_to_int" (t @-> returning int)
 end
+
